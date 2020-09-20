8d207551458e872d24d54c6e8e0a2ae3e1bb4807
jaysonzanarias <jayson.zanarias@gmail.com>
2020-09-21 01:05:07 +0800

    Initial Home and Navbar layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppComponent } from './app.component';
+import { ContentComponent } from './content/content.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProjectListComponent } from './content/portfolio/project-list/project-list.component';
+import { ProjectContainerComponent } from './content/portfolio/project-container/project-container.component';
+import { PortfolioComponent } from './content/portfolio/portfolio.component';
+import { HomeComponent } from './content/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    NavbarComponent,
+    ProjectListComponent,
+    ProjectContainerComponent,
+    PortfolioComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/content/portfolio/portfolio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.css']
+})
+export class PortfolioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
